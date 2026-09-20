318c8bbc7205111524e890a73dabdde1143713ed
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-20 16:45:29 +0500

    Updated code

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
+const connectDB = async () => {
+  await mongoose
+    .connect(process.env.MOGNODB!, {
+      maxPoolSize: 1,
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 45000,
+    })
+    .then((res) => {
+      console.log("Mongodb connected successfully!");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
-const connectDB =  async () => {
-  await mongoose.connect(process.env.MOGNODB!)
-  .then(res => {
-    console.log("Mongodb connected successfully!")
-  })
-  .catch(err => {
-    console.log(err)
-  })
-}
-
-export default connectDB
+export default connectDB;
