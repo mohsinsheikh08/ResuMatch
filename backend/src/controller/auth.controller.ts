342d19012ce3251ceaab6866f1414c0b393fc278
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-22 17:45:07 +0500

    Updated code!

--- backend/src/controller/auth.controller.ts
+++ backend/src/controller/auth.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/auth.model";
 import blackListModel from "../models/blacklist.model";
