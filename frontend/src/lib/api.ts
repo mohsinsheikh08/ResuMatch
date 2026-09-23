d3b47d7f5316164bada286749e5e7f8c69a366c4
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-23 14:24:57 +0500

    update code

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:4000/api/auth",
+    baseURL: `${process.env.NEXT_PUBLIC_API_URL}/api/auth`,
     withCredentials: true
 })
 
