9762f3ce881ac79ccac96f672215bd09190e1c86
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-23 16:13:31 +0500

    Updated code!

--- frontend/src/app/(protected)/dashboard/page.tsx
+++ frontend/src/app/(protected)/dashboard/page.tsx
@@ -6,7 +6,7 @@ const DashboardRouter = () => {
   return (
    <div className="bg-[#1c1c1c] absolute top-0 z-2 sm:p-0  pt-10 w-full min-h-screen  flex items-center justify-center">
     <Link href='/'><p className="text-white absolute top-5  hover:text-[#FF7F00]  active:text-[#FF7F00] active:bg-[#252525] hover:bg-[#252525] bg-[#161616] text-sm transition-all duration-300 ease-in-out cursor-pointer font-semibold px-3 py-2 rounded-lg left-5 sm:top-10 flex gap-1 sm:left-10" ><ArrowLeft /> Back to Home Page </p> </Link>
-      <div className="w-full flex mt-10 sm:m-0 justify-center items-center min-h-screen">
+      <div className="w-full flex my-10 sm:m-0 justify-center items-center min-h-screen">
         <InfoPage />
       </div>
     </div>
