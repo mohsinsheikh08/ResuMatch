31f765f1fad6cdcd898c115db72efabde0a165d2
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-23 17:08:10 +0500

    Updated code!

--- frontend/src/hooks/auth.hooks.ts
+++ frontend/src/hooks/auth.hooks.ts
@@ -1,14 +1,27 @@
-"use client";  
+"use client";
 
 import { useContext } from "react";
 import { authContext } from "../contexts/AuthProvider";
 
 const useAuth = () => {
-    const contexts = useContext(authContext);
-    if (!contexts) {
-        throw new Error("The User is not available!");
-    }
-    return contexts;
+  const context = useContext(authContext);
+  
+  if (!context) {
+    return {
+      user: null,
+      loading: true,
+      error: "",
+      setUser: () => {},
+      setLoading: () => {},
+      setError: () => {},
+      register: async () => {},
+      login: async () => {},
+      logout: async () => {},
+      fetchUser: async () => {},
+    };
+  }
+
+  return context;
 };
 
 export default useAuth;
