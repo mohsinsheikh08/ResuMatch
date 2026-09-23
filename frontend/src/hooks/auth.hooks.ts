2e61d23e016bd26fbbfcb789c33632fc54818e7a
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-23 17:06:50 +0500

    Updated code!

--- frontend/src/hooks/auth.hooks.ts
+++ frontend/src/hooks/auth.hooks.ts
@@ -1,26 +1,14 @@
-"use client";
+"use client";  
 
 import { useContext } from "react";
 import { authContext } from "../contexts/AuthProvider";
 
 const useAuth = () => {
-  const context = useContext(authContext);
-
-  // ✅ Safe default — context na ho to empty object
-  return (
-    context ?? {
-      user: null,
-      loading: true,
-      error: "",
-      setUser: () => {},
-      setLoading: () => {},
-      setError: () => {},
-      register: async () => {},
-      login: async () => {},
-      logout: async () => {},
-      fetchUser: async () => {},
+    const contexts = useContext(authContext);
+    if (!contexts) {
+        throw new Error("The User is not available!");
     }
-  );
+    return contexts;
 };
 
 export default useAuth;
