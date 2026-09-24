720f34919b20f95277e8b8724de34989b598beff
mohsinsheikh08 <mohsinshahidsheikh1@gmail.com>
2026-09-24 17:38:31 +0500

    Update code

--- frontend/src/app/login/page.tsx
+++ frontend/src/app/login/page.tsx
@@ -7,7 +7,7 @@ const LoginRouter = () => {
         <div className=' h-[90%] mx-10 w-100 px-4  flex justify-center  flex-col   py-3 '>
          <h1 className='text-2xl font-semibold justify-center flex  text-[#FF7F00]'>Create Your Account</h1>
          <LoginPage />
-       <Link href='/register'> <p className="text-white flex justify-center gap-1">If you do not have account! <span className="text-[#FF7F00] underline cursor-pointer">Register</span></p></Link>
+       <Link href='/register'> <p className="text-white text-sm flex justify-center gap-1">If you do not have account! <span className="text-[#FF7F00] underline cursor-pointer">Register</span></p></Link>
         </div>  
     </div>
   )
--- frontend/src/app/register/page.tsx
+++ frontend/src/app/register/page.tsx
@@ -8,7 +8,7 @@ const RegisterRouter = () => {
          <h1 className='text-2xl font-semibold justify-center flex  text-[#FF7F00]'>Create Your Account</h1>
          <RegisterPage />
 
-       <Link href='/login'> <p className="text-white flex justify-center gap-1">If you have already account! <span className="text-[#FF7F00] underline cursor-pointer">Login</span></p></Link>
+       <Link href='/login'> <p className="text-white  text-sm flex justify-center gap-1">If you have already account! <span className="text-[#FF7F00] underline cursor-pointer">Login</span></p></Link>
         </div>  
     </div>
   )
